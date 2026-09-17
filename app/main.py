db91a9e5d80fe42ff71c48a4e171bf77ff04d357
silvacarvalho17 <carvalho.paulojsilva@gmail.com>
2026-09-17 13:00:59 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-# write your imports here
-
-
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
     pass
