ee5806160a04cff28244575ee00c4342d0c5c3a7
silvacarvalho17 <carvalho.paulojsilva@gmail.com>
2026-09-17 14:14:57 +0100

    Update hall.py

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,27 +1,21 @@
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
-
 class CinemaHall:
-    def __init__(self, hall_number: int) -> None:
-        self.hall_number = hall_number
+    def __init__(self, number: int) -> None:
+        self.number = number
 
     def movie_session(
         self,
         movie_name: str,
-        customers: list[Customer],
-        cleaning_staff: Cleaner
+        customers: list,
+        cleaning_staff
     ) -> None:
         print(
-            f'"{movie_name}" started in '
-            f"hall number {self.hall_number}."
+            f'"{movie_name}" started in hall number {self.number}.'
         )
 
         for customer in customers:
-            customer.watch_movie(movie=movie_name)
+            customer.watch_movie(movie_name)
 
-        print(f'"{movie_name}" ended.')
-
-        cleaning_staff.clean_hall(
-            hall_number=self.hall_number
-        )
+        cleaning_staff.clean_hall(self.number)
